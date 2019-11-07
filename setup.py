f9be405f00bfca344bccaf936a3d6cd3d25f8f5a
Paul <46288271+raccoondude@users.noreply.github.com>
2019-11-07 16:52:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
   download_url = 'https://github.com/raccoondude/epy/archive/0.7.tar.gz',    # I explain this later on
   keywords = ['API', 'furry', 'e621'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'urllib',
-          'json',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
